1477ee9a5e108a60b864bb2d97f22dcd859fb44a
Hoa Tử Sắc <ard_nguyen@outlook.com.vn>
2021-07-21 23:45:05 +0700

    set seat

--- src/pages/booking/BookingPage.js
+++ src/pages/booking/BookingPage.js
@@ -8,8 +8,7 @@ import { Nav, Dropdown } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './styles.css'
-
-
+import { Box } from '@material-ui/core';
 
 
 
@@ -56,6 +55,29 @@ function PhanTichData(showtime, selectedDay, selectedCineplex, selectedCinema){
 }
 
 
+
+function GetShowTimeSeat(cinema)
+{
+  
+  //let bookedSeat = ticket.map(tic => tic.Seat);
+  let Height = cinema.Height;
+  let Width = cinema.Width;
+  let CinemaSeat = {'row':[]};
+  for (let index = 0; index < Height; index++) {
+      let SeatChar = String.fromCharCode(65+index);
+      for (let j = 0; j < Width; j++) 
+      {
+          const element = SeatChar + (j + 1);
+          CinemaSeat.row.push(element);      
+      }
+  }
+  return CinemaSeat;
+}
+
+
+
+
+
 export default function BookingPage()
 {
   let {id} = useParams();
@@ -79,13 +101,38 @@ export default function BookingPage()
 
   console.log(showtime);
 
+  const test1 = showtime[1];
 
+  const arr0 =[];
+    
+
+  const arr1=[];
+
+  if(test1)
+  {
+      
+    for(let i =0; i < test1.Height ; i++)
+    {
+        arr0.push(String.fromCharCode(65+i));
+    }
+   
+
+    
+    for(let i=0; i < test1.Width; i++){
+        arr1.push(i+1);
+    }
+   
+  }
+
+
+  const seats = GetShowTimeSeat({Height: 12, Width: 12});
+
+  console.log("arr0: ", arr0);
+  console.log("arr1: ", arr1);
 
   const item = showtime.map((ite)=>{
     return(
       <Dropdown.Item onclick={(e) => {
-
-
       }}>
           {ite.DateShow}
       </Dropdown.Item>
@@ -108,16 +155,58 @@ export default function BookingPage()
     )
   });
 
+  const MapSeat = arr0.map((char) => {
+    return(
+      <div className="column">
+        {char}
+      </div>
+    )
+  })
+
+
+
+  console.log("test1: ", test1);
+  console.log("seat: ", seats);
+
+  const renderSeatCode = () => 
+  {
+    if(test1)
+    {
+    const vertical_size= test1.Height;
+    const arr = new Array(vertical_size * test1.Width).fill("");
+    const newArr = arr.map((elem, index) => 
+    {
+      const charI = String.fromCharCode(65 + ~~(index / test1.Width));
+      const number = ~~(index % test1.Width) + 1;
+      return { seat: `${charI}${number}`, available: false };
+    });
+    return newArr;}
+  };
+
+
+  const renderSeat = () => {
+    if(renderSeatCode()!==undefined){
+    return renderSeatCode().map((elm, _index) => {
+      
+      return (
+      <div key={_index} style={{ width: `calc(100%/${test1.Width} - 2rem)`, margin: "1rem" }}>
+        <div class="single-seat">
+          <span class="sit-num">{elm.seat}</span>
+        </div>
+      </div>)
+    });}
+  };
+
     return(
       <div className="booking">
         <div className="selected">
           <Dropdown as={Nav.Item}>
-                <Dropdown.Toggle
+                <Dropdown.Toggle               
                   as={Nav.Link}
                   data-toggle="dropdown"
                   id="dropdown-67443507"
                   variant="default"
-                  className="m-0"
+                  className="lepp"
                 >
                   <span className="d-lg-none ml-1">Date Show</span>
                 </Dropdown.Toggle>
@@ -132,7 +221,7 @@ export default function BookingPage()
                   data-toggle="dropdown"
                   id="dropdown-67443507"
                   variant="default"
-                  className="m-0"
+                  className="lepp"
                 >
                   <span className="d-lg-none ml-1">Cineplexs</span>
                 </Dropdown.Toggle>
@@ -147,7 +236,7 @@ export default function BookingPage()
                   data-toggle="dropdown"
                   id="dropdown-67443507"
                   variant="default"
-                  className="m-0"
+                  className="lepp"
                 >
                   <span className="d-lg-none ml-1">Cinemas</span>
                 </Dropdown.Toggle>
@@ -156,6 +245,16 @@ export default function BookingPage()
                 </Dropdown.Menu>
           </Dropdown>
         </div>
+
+        <div class="screen-wrapper">
+                <div class="seat-area couple">
+                  <div class="seat-line">
+                    {renderSeat()}
+                  </div>
+                </div>
+        </div>
+      
+          
 
       </div>
     );
