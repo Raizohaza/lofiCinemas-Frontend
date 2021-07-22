828b513ab8af269df6ca164835f634478dacd296
Hoa Tử Sắc <ard_nguyen@outlook.com.vn>
2021-07-22 19:45:33 +0700

    fix router login

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -30,8 +30,8 @@ function Routers() {
   return (
     <div className="a">
     <Router>
-      <Header/>
       <LoginRoute/>
+      <Header/>
       <RegisterRoute/>
       <HomeRoute/>
       <ResetRoute/>
